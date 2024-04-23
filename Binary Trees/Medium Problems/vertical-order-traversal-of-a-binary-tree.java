319ead62e19b4a71ccdf592bcc53eaa478523014
Tanvi_Sharma <101709149+TanviSharma2104@users.noreply.github.com>
2024-04-24 00:18:49 +0530

    Update vertical-order-traversal-of-a-binary-tree.java

--- Binary Trees/Medium Problems/vertical-order-traversal-of-a-binary-tree.java
+++ Binary Trees/Medium Problems/vertical-order-traversal-of-a-binary-tree.java
@@ -15,52 +15,55 @@
  */
 
  class Tuple{
-     TreeNode node;
-     int row;
-     int col;
-     public Tuple(TreeNode _node, int _row,int _col){
-         node=_node;
-         row=_row;
-         col=_col;
-     }
+    TreeNode node;
+    int ver;
+    int lev;
+    public Tuple(TreeNode node, int ver, int lev){
+        this.node=node;
+        this.ver=ver;
+        this.lev=lev;
+    }
  }
 class Solution {
     public List<List<Integer>> verticalTraversal(TreeNode root) {
         TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map=new TreeMap<>();
-        Queue<Tuple> q=new LinkedList<Tuple>();
-        q.offer(new Tuple(root,0,0));
-
+        // vertices, level, node
+        Queue<Tuple> q=new LinkedList<>();
+        q.offer(new Tuple(root, 0,0));
         while(!q.isEmpty()){
-            Tuple tuple=q.poll();
-            TreeNode node=tuple.node;
-            int x=tuple.row;
-            int y=tuple.col;
+            Tuple curr=q.remove();
+            TreeNode node=curr.node;
+            int x=curr.ver;
+            int y=curr.lev;
+
             if(!map.containsKey(x)){
-                map.put(x,new TreeMap<>());
+                map.put(x, new TreeMap<>());
+
             }
             if(!map.get(x).containsKey(y)){
                 map.get(x).put(y, new PriorityQueue<>());
             }
-
             map.get(x).get(y).offer(node.val);
 
             if(node.left!=null){
-                q.offer(new Tuple(node.left, x-1 ,y+1));
+                q.offer(new Tuple(node.left, x-1, y+1));
             }
             if(node.right!=null){
-                q.offer(new Tuple(node.right, x+1,y+1));
+                q.offer(new Tuple(node.right, x+1, y+1));
             }
         }
-        List<List<Integer>> list=new ArrayList<>();
-        for (TreeMap < Integer, PriorityQueue < Integer >> ys: map.values()) {
-            list.add(new ArrayList < > ());
-            for (PriorityQueue < Integer > nodes: ys.values()) {
-                while (!nodes.isEmpty()) {
-                    list.get(list.size() - 1).add(nodes.poll());
+
+        List<List<Integer>> ans=new ArrayList<>();
+
+        for(TreeMap<Integer, PriorityQueue<Integer>> li: map.values()){
+            ans.add(new ArrayList<>());
+            for(PriorityQueue<Integer> nodePQ: li.values()){
+                while(!nodePQ.isEmpty()){
+                    ans.get(ans.size()-1).add(nodePQ.poll());
                 }
             }
         }
-        return list;
-        
+        return ans;
+
     }
 }
